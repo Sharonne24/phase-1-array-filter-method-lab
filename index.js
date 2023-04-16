314d02652fe1cc63e6cede515c9056e966c4a142
Sharonne24 <sawuor395@gmail.com>
2023-04-16 20:34:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const searchString = string.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+      const driverName = driver.toLowerCase();
+      return driverName === searchString;
+    });
+  
+    return matchingDrivers;
+  }
+  
+  function fuzzyMatch(drivers, string) {
+        const matchingDrivers = drivers.filter(driver => {
+          return driver.startsWith(string);
+        });
+      
+        return matchingDrivers;
+      }
+      
+  function matchName(drivers, string) {
+        const matchingDrivers = drivers.filter(driver => {
+          return driver.name === string;
+        });
+      
+        return matchingDrivers;
+      }
+      
+
+
